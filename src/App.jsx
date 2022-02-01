26c515baa0b621d0ce6549439dc8880a30a00ac4
marce1724 <marcelaisabelperez1724@gmail.com>
2022-02-01 12:32:03 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react"
+import styled from "@emotion/styled"
+import Frase from "./components/Frase"
+
+const Contenedor = styled.div `
+   display : flex;
+   align-items: center;
+   padding-top: 5rem;
+   flex-direction: column;
+`
+
+const Boton = styled.button`
+   background: -webkit-linear-gradient(top left , #007d35 0%, #007d35 40%, #0f574e 100%);
+   background-size: 300px;
+   font-family: Arial, Helvetica, sans-serif;
+   color: #FFFF;
+   margin-top: 3rem;
+   padding: 1rem 3rem;
+   font-size: 2rem;
+   border: 2px solid black;
+   transition: background-size .8s ease;
+
+    &:hover {
+        cursor: pointer;
+        background-size: 400px;
+    }
+`
+
+function App() {
+
+   const[frase, setFrase] = useState({});
+
+   const consultarApi = async () => {
+      const api = await fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes")
+      const frase = await api.json()
+      setFrase(frase[0])
+   }
+
+   useEffect(() =>{
+      consultarApi()
+   }, [])
+
+  return (
+    <div className="App">
+       <Contenedor>
+         <Frase
+           frase={frase}
+         />
+
+          <Boton
+             onClick={consultarApi}
+          >
+            Get Pharse
+         </Boton>
+       </Contenedor>
+    
+    </div>
+  )
+}
+
+export default App
